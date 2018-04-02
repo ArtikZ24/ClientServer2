8ea56efbe3891536130a0aa9b3b6b8793aba8781
artum <drart24@gmail.com>
2018-04-02 15:13:40 +0300

    changed message

--- Client.py
+++ Client.py
@@ -5,7 +5,7 @@ import socket
 TCP_IP = '127.0.0.1'
 TCP_PORT = 62
 BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
+MESSAGE = "ADD Hello, World!"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
