4828765cb6752bb450f3adc6714ae63507e6e147
artum <drart24@gmail.com>
2018-04-04 11:47:30 +0300

    changed server

--- Client.py
+++ Client.py
@@ -1,19 +1,35 @@
 #!/usr/bin/env python
 
 import socket
+import time
+import string
+from random import *
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 62
 BUFFER_SIZE = 1024
 MESSAGE = "ADD Hello, World!"
 
+
+def id_generator(size=10, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 while True:
-    MESSAGE = raw_input("Command: ")
-    s.send(MESSAGE)
-    data = s.recv(BUFFER_SIZE)
-    print "received data:", data
+    message = raw_input("Command: ")
+    if "AUTO" in message:
+        message_number = message.split(" ")[1]
+        message_delay = message.split(" ")[2]
+        for i in range(0, int(message_number)):
+            message = id_generator()
+            s.send(message)
+            time.sleep(int(message_delay))
+    else:
+        s.send(message)
+        data = s.recv(BUFFER_SIZE)
+        print "received data:", data
 
 s.close()
 
